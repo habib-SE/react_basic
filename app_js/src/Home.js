0a6d04118f82d5eb9f95a37a117dcdca733f576e
habib <hab67603@gmail.com>
2022-11-02 10:42:56 +0500

    add react file

---
+++ app_js/src/Home.js
@@ -0,0 +1,12 @@
+import React,{component} from 'react';
+class Home extends component
+ {
+  render() {
+    return (
+      <div>
+    <p>This is first progrm in React</p>
+      </div>
+    );
+  }
+}
+export default Home;
